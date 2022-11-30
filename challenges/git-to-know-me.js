145b348bd493bba95b3a206e76a3b1343015f54c
Rob Vanarsdall <rvanar@gmail.com>
2022-11-30 12:28:21 -0500

    added git to know me

---
+++ challenges/git-to-know-me.js
@@ -0,0 +1,12 @@
+let person1 = {};
+let person2 = {};
+
+/* 
+
+
+Students will then create an array with everyone's info from the variables they created. 
+
+For the final part -  console log a sentence from the newly created array. 
+"There were  3 . people in our group. Their names were  Rob , Ted,  Fred.  Rob graduated in 1998. Ted's favorite singer is Michael jackson. And Fred does not have any pets."
+
+*/
